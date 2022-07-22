7e87d446e377cf29a5971ed557a08139d69e89ce
Guillermo Rodas <me@guillermorodas.com>
2022-07-22 15:24:19 +0200

    Adding our first story in Storybook

---
+++ stories/Card/Card.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import { options } from './constants'
+
+export const Card = ({
+	children = "I'm card",
+	color = "primary",
+	size = "sm"
+}) => {
+	return <div>
+		{children}
+	</div>
+}
+
+Card.propTypes = {
+	children: PropTypes.node.isRequired,
+	color: PropTypes.oneOf(options.colors),
+	size: PropTypes.oneOf(options.sizes),
+}
+
+export default Card

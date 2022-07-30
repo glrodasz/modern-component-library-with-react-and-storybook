a2bfcdca2b804ffd0d06b6eb756edf08445a81ff
Guillermo Rodas <me@guillermorodas.com>
2022-07-30 15:08:49 +0200

    Global decorator in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,25 @@ import 'minireset.css'
 import '../styles/global.css'
 import '../styles/tokens.css'
 
+const styles = {
+  display: "flex",
+  flexDirection: "column",
+  maxHeight: "auto",
+  justifyContent: "flex-start",
+  alignContent: "flex-start",
+  flexWrap: "wrap",
+  height: "100%",
+  gap: "10px 30px",
+}
+
+export const decorators = [
+  (Story) => (
+    <div style={styles}>
+      <Story />
+    </div>
+  )
+]
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {

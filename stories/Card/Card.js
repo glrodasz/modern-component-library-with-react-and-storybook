fdd0956a32f41dd37513cab16a0540ce3b466545
Guillermo Rodas <me@guillermorodas.com>
2022-07-30 18:05:33 +0200

    Style helpers in Storybook

--- stories/Card/Card.js
+++ stories/Card/Card.js
@@ -1,33 +1,41 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
-import { options } from './constants'
-import classNames from 'classnames'
+import * as React from "react";
+import PropTypes from "prop-types";
+import { options } from "./constants";
+import classNames from "classnames";
 
-import styles from "./Card.module.css"
+import styles from "./Card.module.css";
+import { getClasses } from "../../helpers/styles";
 
 export const Card = ({
-	children,
-	color = "primary",
-	size = "sm",
-	isClickable,
-	isDragable
+  children,
+  color = "primary",
+  size = "sm",
+  isClickable,
+  isDraggable,
 }) => {
-	return <div className={classNames(styles.card, {
-		[styles[`color-${color}`]]: color,
-		[styles[`size-${size}`]]: size,
-		[styles["is-clickable"]]: isClickable,
-		[styles["is-dragable"]]: isDragable
-	})}>
-		{children}
-	</div>
-}
+  const getStyles = getClasses(styles)({
+    color,
+    size,
+  });
+
+  return (
+    <div
+      className={getStyles("card", ["color", "size"], {
+        "is-clickable": isClickable,
+        "is-draggable": isDraggable,
+      })}
+    >
+      {children}
+    </div>
+  );
+};
 
 Card.propTypes = {
-	children: PropTypes.node.isRequired,
-	color: PropTypes.oneOf(options.colors),
-	size: PropTypes.oneOf(options.sizes),
-	isClickable: PropTypes.bool,
-	isDragable: PropTypes.bool
-}
+  children: PropTypes.node.isRequired,
+  color: PropTypes.oneOf(options.colors),
+  size: PropTypes.oneOf(options.sizes),
+  isClickable: PropTypes.bool,
+  isDraggable: PropTypes.bool,
+};
 
-export default Card
+export default Card;
--- stories/Card/Card.stories.js
+++ stories/Card/Card.stories.js
@@ -44,9 +44,9 @@ Clickable.args = {
   isClickable: true,
 };
 
-export const Dragable = Template.bind({});
-Dragable.args = {
-  isDragable: true,
+export const Draggable = Template.bind({});
+Draggable.args = {
+  isDraggable: true,
 };
 
 export const Colors = ListTemplate.bind({});
